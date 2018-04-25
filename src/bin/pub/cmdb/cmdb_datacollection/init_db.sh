a60c41a3237f2ff30589412c387b50326fd01607
chaoyue <chaoyue@juanpi.com>
2018-04-25 10:07:19 +0800

    host model

--- src/bin/pub/cmdb/cmdb_datacollection/init_db.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-set -e
-
-# get local IP.
-localIp=`python ip.py`
-curl -X POST -H 'BK_USER:migrate' -H 'HTTP_BLUEKING_SUPPLIER_ID:0' http://${localIp}:60005/migrate/v3/migrate/community/0
-
-echo ""
--- src/bin/pub/cmdb/cmdb_procserver/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set -e
-killall cmdb_procserver
